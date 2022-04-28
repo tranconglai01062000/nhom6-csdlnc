8ca0aefe02f353576b6e8f3944ad4b093614a256
tranconglai01062000 <85822450+tranconglai01062000@users.noreply.github.com>
2022-04-28 19:49:42 +0700

    Update ProductController.js

--- server/controller/ProductController.js
+++ server/controller/ProductController.js
@@ -1,6 +1,8 @@
 const Product = require("../models/product");
 const Classify = require("../models/classify");
-
+import Product from '../models/product';
+import formidable from 'formidable';
+import fs from 'fs';
 class ProductController {
   //update lai một trường dữ liệu trong sản phẩm
   updateProduct(id, Obj) {
@@ -50,3 +52,41 @@ class ProductController {
 }
 
 module.exports = new ProductController();
+//thêm sản phẩm 
+ export const creat = (req, res) => {
+	 let form = new formidable.IncomingForm();
+	 form.keepExtensions = true;
+	 form.parse(req,(err ,fields,files) => {
+		if(err){
+			return res.status(400).json({
+				error:"Thêm sản phẩm không thành công"
+			})
+		} 
+		const (name, description,price) = fields;
+		if(!name || !description || !price) {
+			return res.status(400).json({
+				error: "Bạn cần nhập đầy đủ thông tin"
+			})
+			
+		}
+		let product = new Product(fields);
+		if (files.photo) {
+			if(files.photo.size > 100000){
+				res.status(400).json({
+					error: "Bạn nên upload ảnh dưới 1MB"
+				});
+			}	
+			product.photo.data = fs.readFileSync(file.photo.path);
+			product.photo.type =files.photo.path;
+		}
+		product.save((err, data)=>{
+			if(err){
+				res.status(400).json({
+					error: "Khong them duoc san pham"
+				});
+			}
+			res.json(data);
+		});
+		
+	 });
+ }
